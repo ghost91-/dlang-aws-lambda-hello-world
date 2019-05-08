6c168559495d893103e5588f192f3447b2f0b1b8
Johannes Loher <johannes.loher@fg4f.de>
2019-05-09 00:57:29 +0200

    switch to std.net.curl instead of requests

--- README.md
+++ README.md
@@ -13,13 +13,6 @@ dub build
 This creates a file `bootstrap.zip` which contains the compiled binary and can
 be used to create a Function on AWS Lambda.
 
-If you compile with LDC, a staticly linked binary is created. You can achieve
-this by running
-```bash
-dub build --compiler=ldc2
-```
-
-
 ## Deploying to AWS Lambda using [serverless](https://serverless.com/)
 
 1. Setup AWS credentials for serverless as described in the
--- source/app.d
+++ source/app.d
@@ -1,4 +1,4 @@
-import requests : Request;
+import std.net.curl : HTTP;
 import std.process : environment;
 
 void main()
@@ -6,19 +6,22 @@ void main()
     immutable awsLambdaRuntimeAPI = environment.get("AWS_LAMBDA_RUNTIME_API");
     while (true)
     {
-        auto request = Request();
-        auto response = request.get(
+        auto requestHTTP = HTTP(
                 "http://" ~ awsLambdaRuntimeAPI ~ "/2018-06-01/runtime/invocation/next");
+        requestHTTP.perform();
+        immutable requestId = requestHTTP.responseHeaders["lambda-runtime-aws-request-id"];
 
-        immutable requestId = response.responseHeaders["lambda-runtime-aws-request-id"];
-
-        request.post("http://" ~ awsLambdaRuntimeAPI ~ "/2018-06-01/runtime/invocation/" ~ requestId ~ "/response",
-                `{
+        auto responseHTTP = HTTP(
+                "http://" ~ awsLambdaRuntimeAPI
+                ~ "/2018-06-01/runtime/invocation/" ~ requestId ~ "/response");
+        responseHTTP.method = HTTP.Method.post;
+        responseHTTP.setPostData(`{
                     "statusCode": 200,
                     "body": "Hello from D on AWS Lambda!",
                     "headers": {
                         "Content-Type": "text/plain"
                     }
-                }`);
+                }`, "application/json");
+        responseHTTP.perform();
     }
 }
